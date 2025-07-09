693a4a71e6d834084cf6db29367906c501859930
JuanBarbosaMora <portelnom0308@gmail.com>
2025-07-09 17:23:34 -0500

    ADD: starter app clone-rappi

---
+++ src/main/java/com/juanba/clone_rappi/CloneRappiApplication.java
@@ -0,0 +1,13 @@
+package com.juanba.clone_rappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneRappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneRappiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanba/clone_rappi/CloneRappiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanba.clone_rappi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneRappiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
